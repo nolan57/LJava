2a2df39291070e92117b9469522786f8f9c19e38
nolan57 <545266262@qq.com>
2020-01-29 09:48:55 +0800

    add draw to thread

---
+++ com/java24hours/java2d/PieFrame.java
@@ -0,0 +1,227 @@
+package com.java24hours.java2d;
+
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedInputStream;
+
+public class PieFrame extends JFrame{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    int sliceNumber=0;
+    ArrayList<PiePanel.PieSlice> slices;
+    PiePanel piePanel;
+    Thread paintThread;
+
+    private void defaultSlice(PiePanel piePanel){
+        for(int idx=0;idx<sliceNumber;idx++){
+            int red=(int)(Math.random()*255);
+            int green=(int)(Math.random()*255);
+            int blue=(int)(Math.random()*255);
+            float sliceSizes=(float)(Math.random()*100);
+            slices.add(piePanel.new PieSlice(new Color(red,green,blue), sliceSizes));
+        }
+    }
+    
+    public PieFrame(int sliceNumber)throws Exception{
+        super("Pie Slices");
+        setLookAndFeel();
+        setBackground(Color.GRAY);
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        slices=new ArrayList<PiePanel.PieSlice>();
+        //System.out.println(Math.pow(2,2));
+
+        if(sliceNumber==0){
+            throw new Exception("Error,no data to draw");
+        }
+        this.sliceNumber=sliceNumber;
+
+        piePanel=new PiePanel();
+        defaultSlice(piePanel);
+        for (PiePanel.PieSlice pieSlice : slices) {
+            piePanel.addPieSlice(pieSlice.sliceColor, pieSlice.size);
+        }
+        add(piePanel);
+        if(paintThread==null){
+            paintThread=new Thread(piePanel);
+            paintThread.start();
+            repaint();
+        }
+    }
+    public PieFrame(){
+        super("Pie Slices");
+        setLookAndFeel();
+        setBackground(Color.GRAY);
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        slices=new ArrayList<PiePanel.PieSlice>();  
+        piePanel=new PiePanel();
+        SetSlices setSlices=new SetSlices(piePanel);
+        add(setSlices.getPiePanel());
+        if(paintThread==null){
+            paintThread=new Thread(this.piePanel);
+            paintThread.start();
+            repaint();
+        }
+    }
+
+    private void setLookAndFeel() {
+        try{
+            UIManager.setLookAndFeel("com.sun.java.plaf.nimbus.NimbusLookAndFeel");
+        }catch(Exception exception){
+            //donothing
+        }
+    }
+    private class SetSlices{
+
+        PiePanel piePanel;
+        BufferedInputStream bin;
+
+        public PiePanel getPiePanel(){
+            return this.piePanel;
+        }
+        public PiePanel.PieSlice setSlice(PiePanel piePanel,int red,int green,int blue,float sliceSizes){
+            PiePanel.PieSlice pp= piePanel.new PieSlice(new Color(red,green,blue), sliceSizes);
+            slices.add(pp);
+            return pp;
+        }
+        public String readConsoleIn() {
+            StringBuilder stringBuilder=new StringBuilder();
+            try{
+                bin=new BufferedInputStream(System.in);
+                int in=0;
+                char inChar;
+                do{
+                    in=bin.read();
+                    inChar=(char)in;
+                    if((in!=-1)&(in!='\n')){
+                        stringBuilder.append(inChar);
+                    }
+                }while((in!=-1)&(in!='\n'));
+                //bin.close();
+                return stringBuilder.toString();
+            }catch (Exception e) {
+                System.out.println("Exception: "+e.getMessage());
+                return null;
+            }
+            
+        }
+        public SetSlices(PiePanel piePanel){
+            boolean again=false;
+            int red=0;
+            int green=0;
+            int blue=0;
+            float size=0;
+            this.piePanel=piePanel;
+            do{
+                System.out.print("Please set the slices number(should be a integer):");
+                String snString=readConsoleIn();
+                try{
+                    sliceNumber=Integer.parseInt(snString);
+                }catch(NumberFormatException nFormatException){
+                    System.out.println("Not a int!");
+                    again=true;
+                }
+                if(!(again)&(sliceNumber<=0)){
+                    System.out.println("Not >0");
+                    again=true;
+                }
+            }while(again);
+
+            System.out.println("Now you should set "+sliceNumber+"slices data");
+            for(int idx=0;idx<sliceNumber;idx++){
+                again=false;
+                /*do{
+                    System.out.print("Please set the slice "+idx+" red number(should be a integer):");
+                    String inString=readConsoleIn();
+                    try{
+                        red=Integer.parseInt(inString);
+                    }catch(NumberFormatException nFormatException){
+                        System.out.println("Not a int!");
+                        again=true;
+                    }
+                    if(!(again)&((red<=0)||(red>255))){
+                        System.out.println("Not >0 & <255");
+                        again=true;
+                    }
+                }while(again);
+
+                again=false;
+                do{
+                    System.out.print("Please set the slice "+idx+" green number(should be a integer):");
+                    String inString=readConsoleIn();
+                    try{
+                        green=Integer.parseInt(inString);
+                    }catch(NumberFormatException nFormatException){
+                        System.out.println("Not a int!");
+                        again=true;
+                    }
+                    if(!(again)&((green<=0)||(green>255))){
+                        System.out.println("Not >0 & <255");
+                        again=true;
+                    }
+                }while(again);
+                again=false;
+                do{
+                    System.out.print("Please set the slice "+idx+" blue number(should be a integer):");
+                    String inString=readConsoleIn();
+                    try{
+                        blue=Integer.parseInt(inString);
+                    }catch(NumberFormatException nFormatException){
+                        System.out.println("Not a int!");
+                        again=true;
+                    }
+                    if(!(again)&((blue<=0)||(blue>255))){
+                        System.out.println("Not >0 & <255");
+                        again=true;
+                    }
+                }while(again);
+                again=false;*/
+                do{
+                    System.out.print("Please set the slice "+idx+" size number(should be a float):");
+                    String inString=readConsoleIn();
+                    try{
+                        size=Float.parseFloat(inString);
+                    }catch(NumberFormatException nFormatException){
+                        System.out.println("Not a int!");
+                        again=true;
+                    }
+                    if(!(again)&(size<=0F)){
+                        System.out.println("Not >0");
+                        again=true;
+                    }
+                }while(again);
+                red=(int)(Math.random()*255);
+                green=(int)(Math.random()*255);
+                blue=(int)(Math.random()*255);    
+                try{
+                        PiePanel.PieSlice pp=this.setSlice(piePanel, red, green, blue, size);
+                        this.piePanel.addPieSlice(pp.sliceColor,pp.size);
+                }catch(NullPointerException uNullPointerException){
+                    System.out.println(uNullPointerException );
+                }
+            }
+            try{
+                bin.close();
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        try{
+            new PieFrame(3);
+            //new PieFrame();
+        }catch(Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
---
+++ com/java24hours/java2d/PiePanel.java
@@ -0,0 +1,150 @@
+package com.java24hours.java2d;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+
+public class PiePanel extends JPanel implements Runnable{
+    ArrayList<PieSlice> pieSlices;
+    ArrayList<Arc2dDrawed> arc2dDraweds;;
+
+    private int sliceCount=0;
+    private float totalSliceSize=0;
+    private float startPoint=0;
+    private float centerOfPie;
+
+
+    public PiePanel(){
+        pieSlices=new ArrayList<>();
+        arc2dDraweds=new ArrayList<>();
+        setSize(500, 500);
+    }
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void addPieSlice(Color sliceColor,float size){
+        pieSlices.add(new PieSlice(sliceColor, size));
+        sliceCount++;
+        totalSliceSize+=size;
+        int frameHeight=getSize().height;
+        int frameWidth=getSize().width;
+        centerOfPie=Math.min(frameHeight,frameWidth);
+    }
+    
+    public class PieSlice{
+        Color sliceColor;
+        float size=0;
+        public PieSlice(Color sliceColor,float size){
+            this.sliceColor=sliceColor;
+            this.size=size;
+        }
+    }
+    private class Arc2dDrawed{
+        int stringX,stringY;
+        int arcX,arcY;
+        int sx,sy;
+        int ex,ey;
+        float sliceSize;
+        float extent;
+        Arc2D.Float sliceToDrawed;
+        public Arc2dDrawed(Arc2D.Float sliceToDrawed,float sliceSize,float extent){
+            this.sliceSize=sliceSize;
+            this.sliceToDrawed=sliceToDrawed;
+            this.extent=extent;
+            Point2D endPoint=this.sliceToDrawed.getEndPoint();
+            Point2D startPoint=this.sliceToDrawed.getStartPoint();
+
+            double ax=startPoint.getX();
+            double bx=endPoint.getX();
+            double ox=centerOfPie;
+            double ay=startPoint.getY();
+            double by=endPoint.getY();
+            double oy=centerOfPie;
+            double R=centerOfPie;
+
+            sx=(int)ax;
+            sy=(int)ay;
+            ex=(int)bx;
+            ey=(int)by;
+
+            double c1=(ax+bx)/2-ox;
+            double c2=(ay+by)/2-oy;
+            double d=Math.pow(c1,2)+Math.pow(c2,2);
+            double e=Math.pow(R,2)/d;
+            double E=Math.sqrt(e);
+
+            //oy=-oy;
+            this.arcX=(int)(E*(ax+bx)/2);
+            this.arcY=(int)(E*(ay+by)/2);
+
+            this.stringX=(int)((arcX+ox)/2);
+            this.stringY=(int)((arcY+oy)/2);
+
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics graphicsComp){
+        super.paintComponent(graphicsComp);
+        Graphics2D graphicsComp2d=(Graphics2D)graphicsComp;
+
+        float compWidth=(float)getSize().width;
+        float compHeight=(float)getSize().height;
+        float miner=Math.min(compHeight,compWidth);
+        this.centerOfPie=miner/2;
+        float leftUpX=0F;
+        float leftUpY=0F;
+
+        pieSlices.forEach(pieSlice->{
+            float extent=pieSlice.size*360F/totalSliceSize;
+            graphicsComp2d.setColor(pieSlice.sliceColor);
+            Arc2D.Float sliceToDrawed=new Arc2D.Float(
+                leftUpX,leftUpY,miner,miner,this.startPoint,extent,Arc2D.Float.PIE
+            );
+            if(arc2dDraweds.size()%sliceCount==0){
+                arc2dDraweds.clear();
+                //arc2dDraweds.clear();
+            }
+            arc2dDraweds.add(new Arc2dDrawed(sliceToDrawed,pieSlice.size,extent));    
+
+            graphicsComp2d.fill(sliceToDrawed);
+            this.startPoint+=extent;
+        });
+        System.out.println(arc2dDraweds.size());
+        arc2dDraweds.forEach(arc2d->{
+            System.out.println("center:("+centerOfPie+","+centerOfPie+")");
+            System.out.println("arc mid points:("+arc2d.arcX+","+arc2d.arcY+")");
+            graphicsComp2d.setColor(Color.BLACK);
+            
+            //graphicsComp2d.drawString((arc2d.sliceSize/totalSliceSize*100)+"%", arc2d.sx, arc2d.sy);
+            //float rx=(arc2d.sx+arc2d.ex)/2;
+            //System.out.println("extent:"+arc2d.extent);
+            float rx=(arc2d.sx+arc2d.ex)/2;
+            float ry=(arc2d.sy+arc2d.ey)/2;
+            if(arc2d.extent>=180){
+                System.out.println("extent>180!");
+                Point dPoint=new Point((int)rx,(int)(ry));
+                Point lineE1=new Point(arc2d.sx,arc2d.sy);
+                Point lineE2=new Point(arc2d.ex,arc2d.ey);
+                Point o=new Point((int)centerOfPie,(int)centerOfPie);
+                Point rxy=DuiChenDL.duiChenDL(dPoint, lineE1, lineE2,o);
+
+                rx=(float)rxy.x;
+                ry=(float)rxy.y;
+            }
+            //graphicsComp2d.drawLine((int)(this.centerOfPie), (int)(this.centerOfPie), (int)(rx), (int)(ry));
+            graphicsComp2d.drawString((arc2d.sliceSize/totalSliceSize*100)+"%", rx, ry);
+            //graphicsComp2d.drawString("@", arc2d.arcX, arc2d.arcY);
+        });
+    }
+
+    @Override
+    public void run() {
+        paintComponent(this.getGraphics().create(0, 0, 500, 500));
+        //repaint();
+    }
+}
